956c38e1e91344a66f40df5b95c7a6e9504530e6
protoid1 <randiardiansyah90@gmail.com>
2025-01-19 08:15:03 +0700

    test publish filament dan edit vercel

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Livewire\Tailwind;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tailwindview', function () {
+    return view('tailwindview');
+});
+
+
+Route::get('/tailwind', Tailwind::class);
+
+
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-
+import { defineConfig } from 'vite'
+import laravel, { refreshPaths } from 'laravel-vite-plugin'
+ 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            input: ['resources/css/app.css', 'resources/js/app.js',
+                'public/css/filament/filament/app.css',
+                'public/css/filament/form/form.css',
+                'public/css/filament/support/support.css'
+            ],
+            refresh: [
+                ...refreshPaths,
+                'app/Livewire/**',
+            ],
         }),
     ],
-});
+})
