e7fea7c17b78121e65fadd5d7afb2dd7e07b1135
protoid1 <randiardiansyah90@gmail.com>
2025-01-19 08:19:23 +0700

    menghilangkan public/css/filament di vite

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,7 @@ import laravel, { refreshPaths } from 'laravel-vite-plugin'
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js',
-                'public/css/filament/filament/app.css',
-                'public/css/filament/form/form.css',
-                'public/css/filament/support/support.css'
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js',],
             refresh: [
                 ...refreshPaths,
                 'app/Livewire/**',
